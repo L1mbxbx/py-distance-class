6c4f862e8a55c236602d33ea86ddaf941d3bc645
Limbxbx <ilyich223@gmail.com>
2025-07-11 10:19:24 +0300

    Fix flake8 errors

--- app/main.py
+++ app/main.py
@@ -30,31 +30,31 @@ class Distance:
             result = self.km / divisor
             return Distance(round(result, 2))
 
-    def __lt__(self, other) -> bool:
+    def __lt__(self, other: "Distance", int, float) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         elif isinstance(other, (int, float)):
             return self.km < other
 
-    def __gt__(self, other) -> bool:
+    def __gt__(self, other: "Distance", int, float) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         elif isinstance(other, (int, float)):
             return self.km > other
 
-    def __eq__(self, other) -> bool:
+    def __eq__(self, other: "Distance", int, float) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         elif isinstance(other, (int, float)):
             return self.km == other
 
-    def __le__(self, other) -> bool:
+    def __le__(self, other: "Distance", int, float) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         elif isinstance(other, (int, float)):
             return self.km <= other
 
-    def __ge__(self, other) -> bool:
+    def __ge__(self, other: "Distance", int, float) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         elif isinstance(other, (int, float)):
